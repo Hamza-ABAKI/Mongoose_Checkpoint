1dda906a6089a57b667665ccdbcd7c4f7c9aafde
Hamza-ABAKI <hamza.abaki@gmail.com>
2021-06-29 10:31:15 +0100

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const connectDB = require('./DB/connectDB')
 const person = require('./Model/person')
